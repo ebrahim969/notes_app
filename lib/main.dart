e86447813e2bfd1368af2916899279ae03f6ca44
ebrahim969 <ebrahimelnemr084@gmail.com>
2023-08-21 22:09:48 +0300

    create simple bloc observer to manage changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
+import 'package:note_app/simple_bloc_observer.dart';
 import 'constants/constant.dart';
-import 'cubits/cubit/add_note_cubit.dart';
+import 'cubits/add_note_cubit/add_note_cubit.dart';
 import 'models/note_model.dart';
 import 'views/home_page.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 void main() async{
+  Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter();
   await Hive.openBox(kNoteBox);
   Hive.registerAdapter(NoteModelAdapter());
