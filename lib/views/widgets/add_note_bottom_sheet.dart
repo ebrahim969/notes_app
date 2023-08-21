a7e566975aecee0ec5169200faf02b422d3685cd
ebrahim969 <ebrahimelnemr084@gmail.com>
2023-08-21 22:04:20 +0300

    add bloc consumer

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:note_app/cubits/cubit/add_note_cubit.dart';
 import 'add_note_form.dart';
-
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
@@ -8,11 +11,26 @@ class AddNoteBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding:const EdgeInsets.all(24),
-      child:const SingleChildScrollView(
-        child: AddNoteForm(),
+      padding: const EdgeInsets.all(24),
+      child: SingleChildScrollView(
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if(state is AddNoteSuccess)
+            {
+              Get.back();
+            }
+            else if(state is AddNoteFaluier)
+            { 
+              
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading ? true : false ,
+              child: const AddNoteForm());
+          },
+        ),
       ),
     );
   }
 }
-
