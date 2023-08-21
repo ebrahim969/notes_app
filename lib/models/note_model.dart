1d411dc294ae043fcabe038cd48044849236f0d9
ebrahim969 <ebrahimelnemr084@gmail.com>
2023-08-21 04:14:39 +0300

    cretae note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final int color;
+  final String date;
+
+  NoteModel(
+      {required this.title,
+      required this.subTitle,
+      required this.color,
+      required this.date});
+}
